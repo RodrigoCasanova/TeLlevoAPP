c97786e665b7fa10849016eda065d117380a38d6
Rodrigo <rodr.casanova@duocuc.cl>
2024-09-12 22:02:36 -0300

    home_creado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // Variable para mantener el estado del viaje activo
+  activeRide: any = null; // Ajusta el tipo según tu estructura de datos
 
-  constructor() {}
+  constructor(private navCtrl: NavController) {}
 
+  // Método para abrir la página de inicio de sesión
+  openLogin() {
+    this.navCtrl.navigateForward('/login');
+  }
+
+  // Método para abrir la página de registro
+  openRegister() {
+    this.navCtrl.navigateForward('/register');
+  }
+
+  // Método para registrar al usuario como conductor
+  registerAsDriver() {
+    // Implementa la lógica para registrar al usuario como conductor
+    console.log('Usuario registrado como conductor');
+  }
+
+  // Método para buscar transporte
+  findRide() {
+    // Implementa la lógica para buscar transporte
+    console.log('Búsqueda de transporte');
+  }
+
+  // Método para cancelar el viaje
+  cancelRide() {
+    // Implementa la lógica para cancelar el viaje
+    console.log('Viaje cancelado');
+  }
+  onClick(){
+    
+  }
 }
